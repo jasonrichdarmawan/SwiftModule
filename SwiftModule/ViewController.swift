7da60ff226d66942b59f0f726d1e0ac834343183
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-11-08 00:21:17 +0700

    Initial Commit

---
+++ SwiftModule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftModule
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 08/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
